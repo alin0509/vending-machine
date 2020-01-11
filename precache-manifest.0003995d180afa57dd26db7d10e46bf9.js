3065c129100e3662ed63ca2dfc4501205931007d
frunza alin <frunza_alin@yahoo.com>
2020-01-11 17:53:46 +0200

    Updates

---
+++ precache-manifest.0003995d180afa57dd26db7d10e46bf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eed335ac52832f877fd2ecbbc06b2a3",
+    "url": "/vending-machine/index.html"
+  },
+  {
+    "revision": "5d707056d048afcf2977",
+    "url": "/vending-machine/static/css/main.5a9f9e37.chunk.css"
+  },
+  {
+    "revision": "3f3c53e6735f3d7e83c5",
+    "url": "/vending-machine/static/js/2.603208ee.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/vending-machine/static/js/2.603208ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d707056d048afcf2977",
+    "url": "/vending-machine/static/js/main.e2bdfaf0.chunk.js"
+  },
+  {
+    "revision": "f930008f357dccf806a0",
+    "url": "/vending-machine/static/js/runtime-main.16517c53.js"
+  }
+]);
